3e53c7440444bbb5812cffa1a50bfb9ba31ebee0
Barsha Chaudhary <64405568+BarshaC@users.noreply.github.com>
2024-01-20 00:45:48 -0500

    Setting env and Variables in JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+//Comment : This is my first Javascript Code
+let firstName = 'Barsha'; 
+let lastName =  'Chaudhary';
+console.log(firstName, lastName);
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name)
+//Cannot contain space or hyphen(-)
+//Generally camelcase is used for naming the variables
+//Are case-sensitive
+
+//const cannot be reassigned
+//Or use let so it can be reassigned
